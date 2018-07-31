51b305f8404a09790909ad0359a820bf01a77af2
P. Strobl <p.strobl.hh@gmail.com>
2018-08-01 00:13:44 +0200

    working on front end

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 "use strict";
 
+// Express setup
 const express = require("express");
 const server = express();
-
 server.use(express.static("public"));
 
-server.listen(8083, () => {
- console.log("Server started at port 8083.");
+// Socket.io setup
+const http = require("http");
+const webServer = http.Server(server);
+const socketIo = require("socket.io");
+const io = socketIo(webServer);
+
+io.sockets.on("connection", userSocket => {
+  console.log(userSocket.id);
 });
 
+// Start WebSocket-Server
+webServer.listen(8083, () => {
+  console.log("Server started at port 8083.");
+});
 
