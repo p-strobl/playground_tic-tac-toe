60d7f88f380fe779c7062abb4b52308d6e011353
P. Strobl <p.strobl.hh@gmail.com>
2018-08-02 00:08:36 +0200

    working on client count

--- public/client.js
+++ public/client.js
@@ -1,3 +1,6 @@
 "use strict";
 
 const socket = io.connect();
+
+
+socket.on("disconnect", () => socket.disconnect());
--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 // Express setup
 const express = require("express");
 const server = express();
-server.use(express.static("public"));
+server.use(express.static(__dirname + "/public"));
 
 // Socket.io setup
 const http = require("http");
@@ -11,12 +11,24 @@ const webServer = http.Server(server);
 const socketIo = require("socket.io");
 const io = socketIo(webServer);
 
+// 
+let clients = [];
+
 io.sockets.on("connection", userSocket => {
   console.log(userSocket.id);
+  clients.push(userSocket.id);
+  console.log(clients);
+  console.log(io.engine.clientsCount);
+
+  userSocket.on("disconnect", userSocket => {
+    clients.splice(clients.indexOf(userSocket), 1);
+    console.log(clients);
+    console.log(io.engine.clientsCount);
+  });
+
 });
 
 // Start WebSocket-Server
 webServer.listen(8083, () => {
   console.log("Server started at port 8083.");
 });
-
