a671ad0d0e43a27c83a1f21f2a4a5f044d1485fb
P. Strobl <p.strobl.hh@gmail.com>
2018-08-09 21:04:40 +0200

    working on room message

--- server.js
+++ server.js
@@ -1,103 +1,52 @@
 "use strict";
 
-// Load Express.js
+// Load require
 const express = require("express");
+const path = require("path");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const Client = require("./user/client.js");
+const Utility = require("./models/utility.js");
 
 // Setup server
-const server = express();
-const webServer = require("http").Server(server);
 const port = 8082;
+const server = express();
+const webServer = http.Server(server);
+const io = socketIo(webServer);
 
-server.use(express.static(__dirname + "/public"));
-
-// Bind Socket.io to webServer
-const io = require("socket.io")(webServer);
-
-// Hold connected clients
 let clients = [];
 
-// Count Clients player
-const countPlayer = () =>
-  clients.filter(client => client.type === "player").length;
-
-// Create client class
-class Client {
-  constructor(socket, type, message) {
-    Object.assign(this, {
-      socket,
-      type,
-      message
-    });
-  };
-
-  static removeFromClients(socket) {
-    clients = clients.filter(client => client.id !== socket.id)
-  };
-
-  addToClients() {
-    clients.push({
-      type: this.type,
-      id: this.socket.id
-    });
-  };
-
-  joinRoom() {
-    this.socket.join(this.type);
-  };
-
-  roomMessage() {
-    io.to(this.type).emit("message", this.message);
-  };
-};
-
-//
-const divideClients = socket => {
-  if (countPlayer() < 2) {
-    var newClient = new Client(socket, "player", "Player-room");
-  } else {
-    var newClient = new Client(socket, "spectator", "Spectator-room");
+// Server class
+class WebServer {
+  constructor() {
+    this.initRoutes();
+    this.start();
   }
-  newClient.addToClients();
-  newClient.joinRoom();
-  newClient.roomMessage();
-};
 
-// 
-const welcomeMessage = (clientCount, socket) => {
-  switch (true) {
-    case clientCount === 1:
-      socket.send("Bitte warten Sie auf Ihren gegner!");
-      // getFooterStatusContent.innerHTML = "Bitte warten Sie auf Ihren gegner!";
-      break;
-    case clientCount === 2:
-      socket.send("Zwei Spieler verbunden. Spiel kann beginnen!");
+  start() {
+    webServer.listen(port, () =>
+      console.log(`Server started at port: ${port}`));
+  }
 
-      // getFooterStatusContent.innerHTML = "Zwei Spieler verbunden. Spiel kann beginnen!";
-      break;
-    case clientCount > 2:
-      socket.send("Es sind bereits zwei Spieler verbunden, bitte versuchen Sie es später noch ein mal.");
-
-      // getFooterStatusContent.innerHTML = "Es sind bereits zwei Spieler verbunden, bitte versuchen Sie es später noch ein mal.";
-      break;
-    default:
+  initRoutes() {
+    server.use(express.static(path.join(__dirname, "public")));
   }
 };
 
+// Socket connecting
 io.sockets.on("connection", socket => {
-  divideClients(socket);
-  // welcomeMessage(io.engine.clientsCount, socket);
+  new Utility(socket, clients).divideClients();
   console.log(clients);
   console.log(io.engine.clientsCount);
 
+  // Socket disconnecting
   socket.on("disconnect", () => {
-    Client.removeFromClients(socket);
-    // welcomeMessage(io.engine.clientsCount, socket);
+    clients = new Client(socket, clients).removeFromClients();
     console.log(clients);
     console.log(io.engine.clientsCount);
   });
 });
 
-// Start WebSocket-Server
-webServer.listen(port, () => {
-  console.log(`Server started at port: ${port}`);
-});
+// Start new server
+new WebServer();
